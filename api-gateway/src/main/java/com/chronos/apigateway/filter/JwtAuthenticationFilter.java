c2038a69b9c3d28272ceac25fdf52a47e312a4d1
Sourasish <sourasishmondal95@gmail.com>
2025-10-28 20:57:23 +0530

    Fixed the API-gateway

---
+++ api-gateway/src/main/java/com/chronos/apigateway/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,63 @@
+package com.chronos.apigateway.filter;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+
+@Component
+public class JwtAuthenticationFilter extends AbstractGatewayFilterFactory<JwtAuthenticationFilter.Config> {
+
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
+    public JwtAuthenticationFilter() {
+        super(Config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> {
+            String authHeader = exchange.getRequest().getHeaders().getFirst("Authorization");
+
+            if(authHeader == null || !authHeader.startsWith("Bearer")) {
+                exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                return exchange.getResponse().setComplete();
+            }
+
+            String token = authHeader.substring(7);
+            try {
+                SecretKey key = Keys.hmacShaKeyFor(jwtSecret.getBytes(StandardCharsets.UTF_8));
+                Claims claims = Jwts.parser()
+                        .verifyWith(key)
+                        .build()
+                        .parseSignedClaims(token)
+                        .getPayload();
+
+                String username = claims.getSubject();
+                String role =claims.get("role", String.class);
+
+                ServerHttpRequest newRequest = exchange.getRequest().mutate()
+                        .header("X-User-Email", username)
+                        .header("X-User-Role", role)
+                        .build();
+
+                return chain.filter(exchange.mutate().request(newRequest).build());
+            } catch (Exception e) {
+                exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                return exchange.getResponse().setComplete();
+            }
+        };
+    }
+
+
+    public static class Config {}
+}
