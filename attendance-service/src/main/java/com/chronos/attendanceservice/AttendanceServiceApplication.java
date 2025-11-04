0f7e4110130b216f63d79711105a00533da9a686
Sourasish <sourasishmondal95@gmail.com>
2025-11-04 20:08:10 +0530

    Implemented circuit breaker, shift, shift swap and attendance testing

--- attendance-service/src/main/java/com/chronos/attendanceservice/AttendanceServiceApplication.java
+++ attendance-service/src/main/java/com/chronos/attendanceservice/AttendanceServiceApplication.java
@@ -8,12 +8,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableDiscoveryClient
-@EnableJpaAuditing
+//@EnableJpaAuditing
 @OpenAPIDefinition(
         info = @Info(
                 title = "Chronos ELAMS REST API Documentation",
